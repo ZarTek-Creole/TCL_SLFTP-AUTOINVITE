cb3761b47397ffced9e30103bd427c99569c6183
ZarTek @ CREOLE <zartek.creole@gmail.com>
2022-12-08 22:26:59 +0100

    Mise a jour pour multi sites sur un reseau irc

--- slftp_autoinvite_cli.tcl
+++ slftp_autoinvite_cli.tcl
@@ -1,14 +1,19 @@
 # Visite https://github.com/ZarTek-Creole/TCL_SLFTP-AUTOINVITE
 namespace eval ::SLINVITE_CLI {
+    # Site name or list of site name separate by space "site1 site2 site3 etc" for this network
     variable SITE_NAME   "<SITENAME_IN_SLFTP>"
     variable IRC_NICK   "<MyNick_Irc_On_SITENAME>"
     variable TO_BOTNAME "";
 }
 proc ::SLINVITE_CLI::init_server {args} {
     if { ${::SLINVITE_CLI::TO_BOTNAME} == "" } {
-        putallbots "SLFTPINVITE ${::SLINVITE_CLI::SITE_NAME} ${::SLINVITE_CLI::IRC_NICK}"
+        foreach { SITENAME } [split ${::SLINVITE_CLI::SITE_NAME}]  {
+            putallbots "SLFTPINVITE ${SITENAME} ${::SLINVITE_CLI::IRC_NICK}";
+        }
     } else {
-        putbot ${::SLINVITE_CLI::TO_BOTNAME} "SLFTPINVITE ${::SLINVITE_CLI::SITE_NAME} ${::SLINVITE_CLI::IRC_NICK}"
+        foreach { SITENAME } [split ${::SLINVITE_CLI::SITE_NAME}]  {
+            putbot ${::SLINVITE_CLI::TO_BOTNAME} "SLFTPINVITE ${SITENAME} ${::SLINVITE_CLI::IRC_NICK}";
+        }
     }
 
 }
